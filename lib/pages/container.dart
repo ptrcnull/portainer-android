98e9c18efb296842021b975582575b9e3e317864
Bjornskjald <github@bjorn.ml>
2018-10-14 18:20:13 +0200

    added container removing

--- lib/pages/container.dart
+++ lib/pages/container.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'containers.dart';
 import '../types/DContainer.dart';
-import '../types/Endpoint.dart';
 
 var colors = {
   'running': Colors.green[300],
@@ -11,42 +10,81 @@ var colors = {
   'restarting': Colors.lightBlue
 };
 
-void refreshContainers (BuildContext context, Endpoint endpoint) {
+void refreshContainer(BuildContext context, DContainer container) async {
+  final endpoint = container.endpoint;
   endpoint.containers = null;
-  endpoint.getContainers();
-  Navigator.of(context).pop();
-  Navigator.of(context).pop();
-  Navigator.of(context).push(getContainersForEndpoint(endpoint));
+  await endpoint.getContainers();
+  if (endpoint.containers.any((_container) => _container.id == container.id)) {
+    Navigator.of(context).pop();
+    Navigator.of(context).push(
+      getContainerPage(
+        endpoint.containers.firstWhere((_container) => _container.id == container.id)
+      )
+    );
+  } else {
+    Navigator.of(context).pop();
+    Navigator.of(context).push(getContainersForEndpoint(endpoint));
+  }
 }
 
 void startContainer (BuildContext context, DContainer container) async {
   await container.start();
   print('Started!');
-  refreshContainers(context, container.endpoint);
+  refreshContainer(context, container);
 }
 
 void stopContainer (BuildContext context, DContainer container) async {
   await container.stop();
   print('Stopped!');
-  refreshContainers(context, container.endpoint);
+  refreshContainer(context, container);
 }
 
 void pauseContainer (BuildContext context, DContainer container) async {
   await container.pause();
   print('Paused!');
-  refreshContainers(context, container.endpoint);
+  refreshContainer(context, container);
 }
 
 void unpauseContainer (BuildContext context, DContainer container) async {
   await container.unpause();
   print('Unpaused!');
-  refreshContainers(context, container.endpoint);
+  refreshContainer(context, container);
 }
 
 void restartContainer (BuildContext context, DContainer container) async {
   await container.restart();
   print('Restarted!');
-  refreshContainers(context, container.endpoint);
+  refreshContainer(context, container);
+}
+
+void deleteContainer (BuildContext context, DContainer container) async {
+  final delete = await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Delete ${container.name}?'),
+        content: Text('This action will irreversibly delete this container.'),
+        actions: <Widget>[
+          new FlatButton(
+            child: new Text("Cancel", style: TextStyle(fontWeight: FontWeight.bold)),
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+          ),
+          new FlatButton(
+            child: new Text("Confirm", style: TextStyle(fontWeight: FontWeight.bold)),
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+          ),
+        ],
+      );
+    }
+  );
+  if (!delete) return;
+  await container.delete();
+  print('Restarted!');
+  refreshContainer(context, container);
 }
 
 MaterialPageRoute getContainerPage(DContainer container) {
@@ -69,38 +107,7 @@ MaterialPageRoute getContainerPage(DContainer container) {
         body: ListView(
           padding: EdgeInsets.all(16.0),
           children: [
-            new ListTile(
-              title: Row(
-                children: <Widget>[
-                  getButton(
-                    Icons.play_arrow,
-                    colors['running'],
-                    onPressed: container.state != 'running' ? () => startContainer(context, container) : null
-                  ),
-                  getButton(
-                    Icons.stop,
-                    colors['exited'],
-                    onPressed: container.state == 'running' ? () => stopContainer(context, container) : null
-                  ),
-                  getButton(
-                    Icons.pause,
-                    colors['paused'],
-                    onPressed: container.state == 'running' ? () => pauseContainer(context, container) : null
-                  ),
-                  getButton(
-                    Icons.play_arrow,
-                    colors['paused'],
-                    onPressed: container.state == 'paused' ? () => unpauseContainer(context, container) : null
-                  ),
-                  getButton(
-                    Icons.refresh,
-                    colors['restarting'],
-                    onPressed: container.state == 'running' ? () => restartContainer(context, container) : null
-                  ),
-                ],
-                
-              ),
-            ),
+            getControls(context, container),
             new ListTile(
               title: Text('State', style: TextStyle(fontSize: 18.0)),
               trailing: Container(
@@ -133,5 +140,44 @@ ButtonTheme getButton (IconData icon, Color color, { Function onPressed }) {
       onPressed: onPressed,
     ),
     padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0)
+  );
+}
+
+ListTile getControls(BuildContext context, DContainer container) {
+  return new ListTile(
+    title: Row(
+      children: <Widget>[
+        getButton(
+          Icons.play_arrow,
+          colors['running'],
+          onPressed: container.isStopped ? () => startContainer(context, container) : null
+        ),
+        getButton(
+          Icons.stop,
+          colors['exited'],
+          onPressed: container.isRunning ? () => stopContainer(context, container) : null
+        ),
+        getButton(
+          Icons.pause,
+          colors['paused'],
+          onPressed: container.isRunning ? () => pauseContainer(context, container) : null
+        ),
+        getButton(
+          Icons.play_arrow,
+          colors['paused'],
+          onPressed: container.isPaused ? () => unpauseContainer(context, container) : null
+        ),
+        getButton(
+          Icons.refresh,
+          colors['restarting'],
+          onPressed: container.isRunning ? () => restartContainer(context, container) : null
+        ),
+        getButton(
+          Icons.delete,
+          colors['exited'],
+          onPressed: container.isStopped ? () => deleteContainer(context, container) : null
+        ),
+      ],
+    ),
   );
 }
--- lib/types/DContainer.dart
+++ lib/types/DContainer.dart
@@ -3,40 +3,49 @@ import 'Endpoint.dart';
 
 class DContainer {
   final String id;
-  final List<dynamic> names;
-  final String image;
+  final List<dynamic> _names;
+  final String _image;
+  String get image => _image.startsWith('sha256:') ? _image.replaceFirst('sha256:', '').substring(0, 12) : _image;
   final String state;
   final String status;
   final Endpoint endpoint;
-  String get name => names[0].substring(1);
-  String get url => '/api/endpoints/${endpoint.id}/docker/containers/$id/';
+  String get name => _names[0].substring(1);
+  String get url => '/api/endpoints/${endpoint.id}/docker/containers/$id';
+  bool get isRunning => state == 'running';
+  bool get isPaused => state == 'paused';
+  bool get isStopped => state == 'created' || state == 'exited' || state == 'dead';
 
   Future<void> start() async {
-    final _response = await MyApp.api.post(url + 'start', {});
+    final _response = await MyApp.api.post(url + '/start', {});
     print(_response);
   }
 
   Future<void> stop() async {
-    final _response = await MyApp.api.post(url + 'stop', {});
+    final _response = await MyApp.api.post(url + '/stop', {});
     print(_response);
   }
 
   Future<void> pause() async {
-    final _response = await MyApp.api.post(url + 'pause', {});
+    final _response = await MyApp.api.post(url + '/pause', {});
     print(_response);
   }
 
   Future<void> unpause() async {
-    final _response = await MyApp.api.post(url + 'unpause', {});
+    final _response = await MyApp.api.post(url + '/unpause', {});
     print(_response);
   }
 
   Future<void> restart() async {
-    final _response = await MyApp.api.post(url + 'restart', {});
+    final _response = await MyApp.api.post(url + '/restart', {});
     print(_response);
   }
 
-  DContainer(this.endpoint, this.id, this.names, this.image, this.state, this.status);
+  Future<void> delete() async {
+    final _response = await MyApp.api.delete(url);
+    print(_response);
+  }
+
+  DContainer(this.endpoint, this.id, this._names, this._image, this.state, this.status);
 
   factory DContainer.fromJson(Endpoint endpoint, Map<String, dynamic> json) {
     return DContainer(endpoint, json['Id'], json['Names'], json['Image'], json['State'], json['Status']);
