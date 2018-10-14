d384d71cd1f83c700e8eb428f9bf1bcd68f264e1
Bjornskjald <github@bjorn.ml>
2018-10-14 12:43:00 +0200

    renamed endpoint page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'api.dart';
 import 'components/asyncList.dart';
-import 'pages/endpointMain.dart';
+import 'pages/endpoint.dart';
 import 'Endpoint.dart';
 
 void main() => runApp(new MyApp());
